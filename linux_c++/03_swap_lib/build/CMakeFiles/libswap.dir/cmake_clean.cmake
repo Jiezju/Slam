ff98909f49e76d158784bfe7a98b20a0f4d80f16
Jiezju <21725100@zju.edu.cn>
2022-09-27 22:56:49 +0800

    [update code] linux c++

---
+++ linux_c++/03_swap_lib/build/CMakeFiles/libswap.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libswap.dir/src/swap.cpp.o"
+  "CMakeFiles/libswap.dir/src/swap.cpp.o.d"
+  "liblibswap.pdb"
+  "liblibswap.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libswap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ linux_c++/03_swap_lib/build/CMakeFiles/libswap.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for libswap.
+# This may be replaced when dependencies are built.
